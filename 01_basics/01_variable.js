81fa5454b689bd3d268d08515bd8caab13ea918b
YashKaushik28 <121500200+YashKaushik28@users.noreply.github.com>
2024-07-04 12:56:29 +0000

    skill to learn github

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountID = 7793233
+let accountEmail = "yashkaushik@google.com"
+var accountPass = "34972"
+accountcity = "Delhi"
+let accountState = 82;
+// accountState = null;
+
+console.log(accountEmail)
+
+console.table([accountID, accountEmail, accountPass, accountcity, accountState])
